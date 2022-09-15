d2f1e3fe7ccfa51a94cef1e8b4c57e8412f362a6
AnthonyLayne <kepheus.ds@gmail.com>
2022-09-14 23:29:36 -0400

    changed migration, filename, installed dev dep

--- data/migrations/20220825175626_initial-migration.js
+++ data/migrations/20220825175626_initial-migration.js
@@ -2,15 +2,15 @@ exports.up = async function up(knex) {
   await knex.schema
     .createTable("notes", (table) => {
       table.increments("id");
-      table.string("username", 60).notNullable();
+      table.string("username", 120).notNullable();
       table.string("title", 200).notNullable();
-      table.string("description").notNullable();
+      table.string("description", 500).notNullable();
       table.timestamp("created_at").defaultTo(knex.fn.now());
     })
     .createTable("users", (table) => {
       table.increments("id");
-      table.string("username", 60).notNullable();
-      table.string("password", 60).notNullable();
+      table.string("username", 120).notNullable().unique();
+      table.string("password", 120).notNullable();
     });
 };
 
