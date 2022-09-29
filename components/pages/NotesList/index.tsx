1b8aa1984b2247f6ebdcd38c976a15eee8bde707
AnthonyLayne <kepheus.ds@gmail.com>
2022-09-28 20:21:45 -0400

    wip components/pages

--- components/pages/NotesList/index.tsx
+++ components/pages/NotesList/index.tsx
@@ -1,25 +1,31 @@
-// import { useNotesContext } from "context/notesContext";
+// import { useNotesContext } from "context/notsesContext";
 
 export function NotesList() {
-  // const {} = useNotesContext();
+  // const { handleGetNotes } = useNotesContext();
 
-  // TODO: get userId off of token, build a getNotes handler func in notes context, give the id to the func to get the usersnotes
-  // TODO: list out the users notes
-  // TODO: create an onclick that opens the note in a modal, where it can be edited
-  // TODO: modal should have, a close button, that should do a PUT req and close at the same time, and a last edited time
-  // TODO: see if you can figure out how to pin notes at the top of the list
-  // TODO: need a delete button, consider a small hamburger icon to appear when hovering over a note, with delete opt inside.
-  // TODO: when the hamburger icon is clicked a small modal opens
-  // TODO: need a + button to create a new note
+  // const id = localStorage.getItem("token");
+  // console.log(id);
+
+  // const notes = handleGetNotes();
+
   return (
     <div className="notesListWrapper">
       <h2 className="header">Your Notes:</h2>
       {/*
       {Object.entries(notes).map(([id, note]) => (
         <Link key={id} to={`/edit/${id}`} className="note">
-          {note.title}
+        {note.title}
         </Link>
       ))} */}
     </div>
   );
 }
+
+// TODO: get userId off of token, build a getNotes handler func in notes context, give the id to the func to get the usersnotes
+// TODO: list out the users notes
+// TODO: create an onclick that opens the note in a modal, where it can be edited
+// TODO: modal should have, a close button, that should do a PUT req and close at the same time, and a last edited time
+// TODO: see if you can figure out how to pin notes at the top of the list
+// TODO: need a delete button, consider a small hamburger icon to appear when hovering over a note, with delete opt inside.
+// TODO: when the hamburger icon is clicked a small modal opens
+// TODO: need a + button to create a new note
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,7 @@
-// import Image from "next/image";
-// import styles from "../styles/Home.module.css";
-
 import { HomePage } from "components/pages/HomePage";
 
 function Home() {
-  return (
-    <div>
-      <HomePage />
-    </div>
-  );
+  return <HomePage />;
 }
 
 export default Home;
