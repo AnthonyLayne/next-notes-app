8d2177f0c6367fc6e877a18e19d7e70a294b2cd4
AnthonyLayne <kepheus.ds@gmail.com>
2022-10-07 19:41:00 -0400

    added css types

--- @types/react/index.d.ts
+++ @types/react/index.d.ts
@@ -3,5 +3,7 @@ import React, { JSX } from "react";
 declare module "react" {
   export interface CSSProperties extends React.CSSProperties {
     "--i"?: number;
+    "--standardTransitionTime"?: string;
+    "--modalTransitionTime"?: string;
   }
 }
