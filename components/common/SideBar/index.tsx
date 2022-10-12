810e8ffacdb2cbe81d64de40d029f8e9b2c518c7
AnthonyLayne <kepheus.ds@gmail.com>
2022-10-11 22:51:04 -0400

    finish sidbar

--- components/common/SideBar/index.tsx
+++ components/common/SideBar/index.tsx
@@ -1,42 +1,65 @@
-import { useState, useCallback } from "react";
+import { CSSProperties, useState, useCallback, useRef, useEffect } from "react";
+import { useRouter } from "next/router";
 import Link from "next/link";
 import cx from "classnames";
 
 // Context
 import { useSidebarContext } from "context/sidebarContext";
 
+// Helpers
+import { getCSSNumberProperty } from "styles/utils";
+
 // Styles
 import styles from "./styles.module.css";
 
+const LINKS = [
+  { href: "/notes", imgSrc: "/icons/filled-note.svg", name: "Notes" },
+  { href: "/notes/archive", imgSrc: "/icons/archive.svg", name: "Archive" },
+  { href: "/notes/trash", imgSrc: "/icons/trash.svg", name: "Trash" },
+];
+
 export function SideBar() {
+  const { pathname, query } = useRouter();
+  const literalPathname = pathname.replace("[category]", query.category as string);
+
   const { sidebarExpanded } = useSidebarContext();
   const [temporarilyExpanded, setTemporarilyExpanded] = useState(false);
 
   const handleTemporaryExpand = useCallback(() => setTemporarilyExpanded(true), []);
 
+  // ---- Handle Img Width for Sidebar Collapsed ----------------------------------------------------------------------
+  const linkRef = useRef<HTMLAnchorElement>(null);
+  const imgRef = useRef<HTMLImageElement>(null);
+
+  const [shrinkWidth, setShrinkWidth] = useState("0px");
+
+  useEffect(() => {
+    const linkPadding = getCSSNumberProperty("padding-left" as keyof CSSProperties, linkRef.current);
+    const imgWidth = getCSSNumberProperty("width", imgRef.current);
+    setShrinkWidth(`${linkPadding * 2 + imgWidth}px`);
+  }, [sidebarExpanded, temporarilyExpanded]);
+  // ------------------------------------------------------------------------------------------------------------------
+
   return (
     <div
       className={cx(styles.sideBarWrapper, { [styles.shrink]: !sidebarExpanded && !temporarilyExpanded })}
+      style={{ "--shrinkWidth": shrinkWidth }}
       onFocus={handleTemporaryExpand}
       onMouseOver={handleTemporaryExpand}
       onMouseLeave={() => setTemporarilyExpanded(false)}
       onBlur={() => setTemporarilyExpanded(false)}
     >
-      <Link href="/notes">
-        <a>
-          <img src="/icons/filled-note.svg" alt="" /> Notes
-        </a>
-      </Link>
-      <Link href="/archive">
-        <a>
-          <img src="/icons/archive.svg" alt="" /> Archive
-        </a>
-      </Link>
-      <Link href="/trash">
-        <a>
-          <img src="/icons/trash.svg" alt="" /> Trash
-        </a>
-      </Link>
+      {LINKS.map(({ href, imgSrc, name }, i) => (
+        <Link key={href} href={href}>
+          <a
+            ref={i === 0 ? linkRef : null}
+            className={cx(styles.link, { [styles.selected]: href === literalPathname })}
+          >
+            <img ref={i === 0 ? imgRef : null} src={imgSrc} alt="" />
+            {name}
+          </a>
+        </Link>
+      ))}
     </div>
   );
 }
