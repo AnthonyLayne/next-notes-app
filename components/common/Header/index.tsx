faf7b4816bd2fc471511b5ddb71206fcf3c50e72
AnthonyLayne <kepheus.ds@gmail.com>
2022-10-11 22:45:48 -0400

    header cmp

--- components/common/Header/index.tsx
+++ components/common/Header/index.tsx
@@ -3,42 +3,49 @@ import Link from "next/link";
 // Context
 import { useAuthContext } from "context/authContext";
 import { useSidebarContext } from "context/sidebarContext";
+import { useNotesContext } from "context/notesContext";
 
 // Components
-import { Input } from "../Input";
+import { Input } from "components/common/Input";
+import { Button } from "components/common/Button";
+import { Hamburger } from "components/common/Hamburger";
 
 import styles from "./styles.module.css";
 
 export function Header() {
   const { handleSignOut } = useAuthContext();
-  const { toggleSidebar } = useSidebarContext();
+  const { toggleSidebar, sidebarExpanded } = useSidebarContext();
+  const { searchString, setSearchString } = useNotesContext();
 
   return (
     <div className={styles.headerWrapper}>
-      <button type="button" onClick={toggleSidebar}>
-        (=)
-      </button>
+      <div>
+        <Hamburger handleClick={toggleSidebar} isOpen={sidebarExpanded} />
 
-      <h1 className={styles.noteHeader}>Hold</h1>
+        <h1 className={styles.noteHeader}>Hold</h1>
 
-      <Link href="/notes?noteId=0">
-        <a className={styles.newButton}>
-          <img src="/icons/new-note.svg" alt="" />
-        </a>
-      </Link>
-      <Input
-        id="search"
-        name="search"
-        ariaLabel="search"
-        label="Search"
-        value=""
-        className={styles.search}
-        input={{ type: "search" }}
-      />
+        <Link href="/notes?noteId=0">
+          <a className={styles.newButton}>
+            <img src="/icons/new-note.svg" alt="" />
+          </a>
+        </Link>
+        <Input
+          id="search"
+          name="search"
+          ariaLabel="search"
+          label="Search"
+          value={searchString}
+          onChange={({ target: { value } }) => setSearchString(value)}
+          className={styles.search}
+          input={{ type: "search" }}
+        />
+      </div>
 
-      <button type="button" className={styles.signOut} onClick={handleSignOut}>
-        Sign Out
-      </button>
+      <div>
+        <Button version="primary" type="button" onClick={handleSignOut}>
+          Sign Out
+        </Button>
+      </div>
     </div>
   );
 }
