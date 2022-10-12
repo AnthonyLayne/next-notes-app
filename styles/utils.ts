42abb94ac0ba60b2b047d6d439b0f36d3fc0805b
AnthonyLayne <kepheus.ds@gmail.com>
2022-10-11 23:05:08 -0400

    whoops, should have gone with earlier commit

--- styles/utils.ts
+++ styles/utils.ts
@@ -1,7 +1,15 @@
 import { CSSProperties } from "react";
 
-export const getCSSNumberProperty = (property: keyof CSSProperties) =>
-  Number(
-    // SSR Check
-    typeof window !== "undefined" ? getComputedStyle(document.body).getPropertyValue(property).replace(/\D+/g, "") : 0
-  );
+export const getCSSNumberProperty = (property: keyof CSSProperties, element?: Nullable<Element>) => {
+  // SSR Check
+  if (typeof window === "undefined") return 0;
+
+  const prop = getComputedStyle(element || document.body).getPropertyValue(property);
+  const match = prop.match(/[\d.]+/g) || [0];
+
+  return Number(match[0]);
+};
+
+export const getCSSStringProperty = (property: keyof CSSProperties, element?: Nullable<Element>) =>
+  // SSR Check
+  typeof window !== "undefined" ? getComputedStyle(element || document.body).getPropertyValue(property) : "";
